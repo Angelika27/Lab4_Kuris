5ccc5ee9369ef19ace3e3a8ac86ff873780ea2f2
Angelika27 <76405959+Angelika27@users.noreply.github.com>
2024-01-02 22:39:44 +0200

    Update PalindromeCheckerTest.java

--- src/test/java/org/Lab4_Kuris/PalindromeCheckerTest.java
+++ src/test/java/org/Lab4_Kuris/PalindromeCheckerTest.java
@@ -1,4 +1,34 @@
-import static org.junit.jupiter.api.Assertions.*;
+package org.Lab4_Kuris;
+
+import java.util.List;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 class PalindromeCheckerTest {
-  
-}
+
+    @Test
+    public void testPalindromeWords() {
+        assertTrue(PalindromeChecker.isPalindrome("radar"));
+        assertTrue(PalindromeChecker.isPalindrome("level"));
+        assertTrue(PalindromeChecker.isPalindrome("Deified"));
+    }
+
+    private void assertTrue(boolean radar) {
+    }
+
+    @Test
+    public void testNonPalindromeWords() {
+        assertFalse(PalindromeChecker.isPalindrome("hello"));
+        assertFalse(PalindromeChecker.isPalindrome("world"));
+        assertFalse(PalindromeChecker.isPalindrome("Java"));
+    }
+
+    private void assertFalse(boolean hello) {
+    }
+
+    @Test
+    public void testEmptyString() {
+        assertTrue(PalindromeChecker.isPalindrome(""));
+    }
+
+
